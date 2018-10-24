d3f7df4e9cc740f267c6d32fad68fb6b6bd4a7a0
Luciano Fernandez Oria <l_fernandez_oria@unc.edu.ar>
2018-10-24 11:07:09 -0300

    Add new file

---
+++ br_generator.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module br_generator(
+    input clock,
+	 output reg tick
+	 );
+
+	reg [7:0] ciclos=8'b0;
+	
+	/*initial 
+		ciclos=0;*/
+
+	always @(posedge clock)
+		begin
+			if(ciclos==163)
+				begin
+					tick=1;
+					ciclos=0;
+				end
+			else
+				begin
+					ciclos=ciclos+1;
+					tick=0;
+				end
+		end	
+	
+endmodule
